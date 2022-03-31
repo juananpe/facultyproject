f43e1b6ce7af726aa9f80747aa4a9108d4b57f3a
isad <isad@ehu.eus>
2022-03-31 11:59:32 +0200

    Remove the enrolledIn association as it is no longer needed

--- src/main/java/faculty/project/domain/Student.java
+++ src/main/java/faculty/project/domain/Student.java
@@ -7,46 +7,37 @@ import java.util.Collection;
 @Entity
 public class Student extends User {
 
-    private int earnedCredits = 0;
+  private int earnedCredits = 0;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
-    private Collection<Subject> enrolledIn;
-
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
-    private Collection<AcademicRecord> academicRecords;
+  @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+  private Collection<AcademicRecord> academicRecords;
 
 
-    public Student(String userName, String password, String completeName, String email, String address, String phoneNumber) {
-        super(userName, password, completeName, email, address, phoneNumber);
+  public Student(String userName, String password, String completeName, String email, String address, String phoneNumber) {
+    super(userName, password, completeName, email, address, phoneNumber);
 
-        enrolledIn = new ArrayList<>();
-        academicRecords = new ArrayList<>();
-    }
+    academicRecords = new ArrayList<>();
+  }
 
-    public Student() {
+  public Student() {
 
-    }
+  }
 
 
-    public Collection<Subject> getEnrolledIn() {
-        return enrolledIn;
-    }
+  public void enroll(Subject subject) {
+    academicRecords.add(new AcademicRecord(subject, this));
+  }
 
-    public void enroll(Subject subject){
-        enrolledIn.add(subject);
-        academicRecords.add(new AcademicRecord(subject, this));
-    }
+  public int getEarnedCredits() {
+    return earnedCredits;
+  }
 
-    public int getEarnedCredits() {
-        return earnedCredits;
-    }
+  public void setEarnedCredits(int earnedCredits) {
+    this.earnedCredits = earnedCredits;
+  }
 
-    public void setEarnedCredits(int earnedCredits) {
-        this.earnedCredits = earnedCredits;
-    }
-
-    public Collection<AcademicRecord> getAcademicRecords() {
-        return academicRecords;
-    }
+  public Collection<AcademicRecord> getAcademicRecords() {
+    return academicRecords;
+  }
 
 }
